29b0c6200923952a5452379feb2c0ed566cf827e
imac141828 <imac141828@imac141828s-iMac.local>
2024-11-06 10:39:06 +0100

    adding frontend url for backend

--- passport-setup.js
+++ passport-setup.js
@@ -3,31 +3,31 @@ import GoogleStrategy from 'passport-google-oauth20';
 import User from './models/user.js'; 
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://fitness-tracker-api-backends.onrender.com/users/auth/google/callback",
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      const existingUser = await User.findOne({ googleId: profile.id });
-      if (existingUser) {
-        return done(null, existingUser);
-      }
-
-      const newUser = new User({
-        googleId: profile.id,
-        username: profile.displayName,
-        email: profile.emails[0].value,
-      });
-
-      await newUser.save();
-      done(null, newUser);
-    } catch (error) {
-      console.error('Error during user creation:', error);
-      done(error, null);
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "https://fitness-tracker-api-backends.onrender.com/users/auth/google/callback",
+},
+async (accessToken, refreshToken, profile, done) => {
+  console.log(profile); 
+  try {
+    const existingUser = await User.findOne({ googleId: profile.id });
+    if (existingUser) {
+      return done(null, existingUser);
     }
+
+    const newUser = new User({
+      googleId: profile.id,
+      username: profile.displayName,
+      email: profile.emails[0].value,
+    });
+
+    await newUser.save();
+    done(null, newUser);
+  } catch (error) {
+    console.error('Error during user creation:', error);
+    done(error, null);
   }
-));
+}));
 
 passport.serializeUser((user, done) => {
   done(null, user.id); 
