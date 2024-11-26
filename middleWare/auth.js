ec9b4f72d90e9e2d6c8f4bb4d78b713d1e7e52c6
imac141828 <imac141828@imac141828s-iMac.local>
2024-11-27 00:39:32 +0100

    solvingisues

--- middleWare/auth.js
+++ middleWare/auth.js
@@ -3,27 +3,29 @@ import User from "../models/user.js";
 
 const auth = async (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
-  console.log("Received Token:", token); 
+
   if (!token) {
     return res.status(401).json({ error: "Authentication required" });
   }
-  
+
   try {
+    console.log("Received Token:", token);  
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded Token:", decoded);  
-  
+
     const user = await User.findById(decoded.user.id);
     if (!user) {
       return res.status(401).json({ error: "User not found" });
     }
-  
-    req.user = { ...user.toObject(), id: user._id };
+
+    const { password, ...restUser } = user.toObject(); 
+    req.user = { ...restUser, id: restUser._id };
+
     next();
   } catch (err) {
     console.error("Authentication error:", err);
     return res.status(401).json({ error: "Invalid token", details: err.message });
   }
-  
 };
 
 export default auth;
